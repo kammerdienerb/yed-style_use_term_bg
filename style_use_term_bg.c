a3c326f9295437ed326fe4b3c3b8334760757a61
Brandon Kammerdiener <kammerdienerb@MacBook-Pro-7.local>
2022-07-20 17:53:23 -0400

    fix

--- style_use_term_bg.c
+++ style_use_term_bg.c
@@ -27,6 +27,8 @@ void style_use_term_bg(int n_args, char **args) {
     s->active_border.bg   = 0;
     s->inactive.bg        = 0;
     s->inactive_border.bg = 0;
+
+    yed_activate_style(s->_name);
 }
 
 void style_term_bg(int n_args, char **args) {
